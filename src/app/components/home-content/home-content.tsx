22acb7aeeb68dacb108ef9ead81589dfd2c95b59
Monica Salamanca <moni.salamanca@gmail.com>
2025-03-24 20:51:03 -0400

    feat: home content moved to desktop view

--- src/app/components/home-content/home-content.tsx
+++ src/app/components/home-content/home-content.tsx
@@ -1,24 +1,30 @@
 "use client";
 
-import { BlogPost } from "@/api/types";
-import Hero from "../hero/hero";
-
-import s from "./home-content.module.scss";
+import { useEffect, useState } from "react";
 import Image from "next/image";
 import Link from "next/link";
+import { useRouter } from "next/navigation";
+import { BlogPost } from "@/api/types";
+import Hero from "../hero/hero";
 import { FontAwesomeIcon } from "@fortawesome/react-fontawesome";
 import {
   faBookOpen,
   faCompass,
   faNewspaper,
+  faRightLong,
   faRotateRight,
 } from "@fortawesome/free-solid-svg-icons";
+
+import s from "./home-content.module.scss";
 
 const HomeContent = ({
   latestBlogPosts,
 }: {
   latestBlogPosts: BlogPost[] | null;
 }) => {
+  const [isMobile, setIsMobile] = useState(false);
+  const router = useRouter();
+
   const formatDate = (dateString: string): string => {
     const options: Intl.DateTimeFormatOptions = {
       year: "numeric",
@@ -28,12 +34,31 @@ const HomeContent = ({
     const date = new Date(dateString);
     return new Intl.DateTimeFormat("en-US", options).format(date);
   };
+
   const handleReload = () => {
     window.location.reload();
   };
+
   const formatUrl = (url: string): string => {
     return url.replace(/\s+/g, "-").toLowerCase();
   };
+
+  useEffect(() => {
+    const handleResize = () => {
+      setIsMobile(window.innerWidth < 768);
+    };
+
+    // Run once on mount
+    handleResize();
+    window.addEventListener("resize", handleResize);
+    return () => window.removeEventListener("resize", handleResize);
+  }, []);
+
+  // Handle click manually on mobile
+  const handleClick = (url: string) => {
+    if (isMobile) router.push(url);
+  };
+
   return (
     <main className={s.container}>
       <Hero
@@ -46,26 +71,41 @@ const HomeContent = ({
       {latestBlogPosts ? (
         <section className={s.latestBlogPosts}>
           <h2>Latest Posts</h2>
-          {latestBlogPosts.map((blogPost) => (
-            <Link
-              key={blogPost.id}
-              className={s.latestBlogPost}
-              href={formatUrl(`${blogPost.category}/${blogPost.title}`)}
-            >
-              <Image
-                className={s.thumbnail}
-                src={blogPost.imageUrl}
-                alt="Blog Post Thumbnail"
-                width={234}
-                height={156}
-              />
-              <div className={s.contentWrapper}>
-                <small>{formatDate(blogPost.publishedAt)}</small>
-                <h3>{blogPost.title}</h3>
-                <p>{blogPost.description}</p>
+          <div className={s.wrapper}>
+            {latestBlogPosts.map((blogPost) => (
+              <div
+                key={blogPost.id}
+                className={s.latestBlogPost}
+                onClick={() =>
+                  handleClick(
+                    formatUrl(`${blogPost.category}/${blogPost.title}`)
+                  )
+                }
+              >
+                <Image
+                  className={s.thumbnail}
+                  src={blogPost.imageUrl}
+                  alt="Blog Post Thumbnail"
+                  width={234}
+                  height={156}
+                />
+                <div className={s.contentWrapper}>
+                  <small>{formatDate(blogPost.publishedAt)}</small>
+                  <h3>{blogPost.title}</h3>
+                  <p>{blogPost.description}</p>
+                  <Link
+                    href={`/${formatUrl(
+                      `${blogPost.category}/${blogPost.title}`
+                    )}`}
+                    className={s.readMore}
+                  >
+                    Read More
+                    <FontAwesomeIcon icon={faRightLong} className={s.icon} />
+                  </Link>
+                </div>
               </div>
-            </Link>
-          ))}
+            ))}
+          </div>
         </section>
       ) : (
         <section className={s.latestBlogPosts}>
