714f611195a3beac4cd62f66fa746894788471f2
Monica Salamanca <moni.salamanca@gmail.com>
2025-04-20 19:34:14 -0400

    fix: error on countries

--- src/app/[categorySlug]/[blogSlug]/page.tsx
+++ src/app/[categorySlug]/[blogSlug]/page.tsx
@@ -2,15 +2,13 @@ import { fetchBlogPost } from "@/api/client";
 import BlogContent from "@/components/blog-content/blog-content";
 import NotFoundPage from "../../not-found";
 
-type Params = {
-  categorySlug: string;
-  blogSlug: string;
-};
-
-const BlogPostPage = async ({ params }: { params: Params }) => {
-  const { categorySlug, blogSlug } = params;
-  const postArray = await fetchBlogPost(categorySlug, blogSlug);
-  console.log("postArray", postArray);
+const BlogPostPage = async ({
+  params,
+}: {
+  params: Promise<{ category: string; slug: string }>;
+}) => {
+  const { category, slug } = await params;
+  const postArray = await fetchBlogPost(category, slug);
 
   if (!postArray || postArray.length === 0) return <NotFoundPage />;
 
@@ -20,3 +18,17 @@ const BlogPostPage = async ({ params }: { params: Params }) => {
 };
 
 export default BlogPostPage;
+
+// // ✅ And here's your static params builder
+// export async function generateStaticParams() {
+//   const res = await fetch(
+//     `${process.env.STRAPI_URL}/api/blogs?fields[0]=slug&populate[category][fields][0]=slug`
+//   );
+//   const data = await res.json();
+
+//   // eslint-disable-next-line @typescript-eslint/no-explicit-any
+//   return data.data.map((post: any) => ({
+//     category: post.attributes.category.data.attributes.slug,
+//     slug: post.attributes.slug,
+//   }));
+// }
--- src/app/[categorySlug]/page.tsx
+++ src/app/[categorySlug]/page.tsx
@@ -2,13 +2,15 @@ import { notFound } from "next/navigation";
 import { fetchCountry } from "@/api/client";
 import CountryLander from "@/components/country-lander/country-lander";
 
-type Params = {
-  categorySlug: string;
-};
-
-const BlogPostPage = async ({ params }: { params: Params }) => {
-  const { categorySlug } = await params;
-  const country = await fetchCountry(categorySlug);
+const BlogPostPage = async ({
+  params,
+}: {
+  params: Promise<{
+    category: string;
+  }>;
+}) => {
+  const { category } = await params;
+  const country = await fetchCountry(category);
   // In here we either return the data based on country or other category
   // So first we will have a list of other categories
   // and we will check if the category exists in the list
