80aed271ab42a913524cf29ba9a0f304f38262c4
Monica Salamanca <moni.salamanca@gmail.com>
2025-05-22 20:17:30 -0400

    chore: redesigned the countries menu

--- src/app/components/header/menu/menu.tsx
+++ src/app/components/header/menu/menu.tsx
@@ -1,19 +1,34 @@
-import { FC } from "react";
+import { FC, useEffect, useRef, useState } from "react";
 import Link from "next/link";
 import NavLink from "./nav-link/nav-link";
 import { GroupedCountries } from "@/api/types";
 import { FontAwesomeIcon } from "@fortawesome/react-fontawesome";
-import { faEarthAsia } from "@fortawesome/free-solid-svg-icons";
+import { faChevronDown, faChevronUp } from "@fortawesome/free-solid-svg-icons";
 
 import s from "./menu.module.scss";
 
 const Menu: FC<{ destinations: GroupedCountries | null }> = ({
   destinations,
 }) => {
+  const [isDestinationsMenuOpen, setIsDestinationsMenuOpen] = useState(false);
+  const menuRef = useRef<HTMLLIElement>(null);
   const capitalize = (country: string) => {
     if (!country) return "";
     return country.charAt(0).toUpperCase() + country.slice(1).toLowerCase();
   };
+
+  useEffect(() => {
+    if (!isDestinationsMenuOpen) return;
+
+    const handleClickOutside = (event: MouseEvent) => {
+      if (menuRef.current && !menuRef.current.contains(event.target as Node)) {
+        setIsDestinationsMenuOpen(false);
+      }
+    };
+    document.addEventListener("mousedown", handleClickOutside);
+    return () => document.removeEventListener("mousedown", handleClickOutside);
+  }, [isDestinationsMenuOpen]);
+
   return (
     <nav className={s.container}>
       <ul>
@@ -22,45 +37,49 @@ const Menu: FC<{ destinations: GroupedCountries | null }> = ({
             Home
           </NavLink>
         </li>
-        <li>
+        <li
+          ref={menuRef}
+          className={s.dropdown}
+          onClick={() => setIsDestinationsMenuOpen((prev) => !prev)}
+          tabIndex={0}
+        >
           <div className={s.menuItem}>
             Destinations
-            <div className={s.destinations}>
-              <div className={s.destinationsWrapper}>
-                {destinations &&
-                  Object.entries(destinations).map(([continent, countries]) => (
-                    <div className={s.continent} key={continent}>
-                      <div className={s.wrapper}>
-                        <h3>
-                          <FontAwesomeIcon
-                            icon={faEarthAsia}
-                            className={s.icon}
-                          />
-                          {continent}
-                        </h3>
-                        <ul>
-                          {countries.map((country) => (
-                            <li key={country}>
-                              <Link
-                                aria-label={`Read more about ${country}`}
-                                href={`/${country.replace(" ", "-")}`}
-                              >
-                                {capitalize(country)}
-                              </Link>
-                            </li>
-                          ))}
-                        </ul>
-                      </div>
-                    </div>
-                  ))}
+            <FontAwesomeIcon
+              icon={isDestinationsMenuOpen ? faChevronUp : faChevronDown}
+              className={s.icon}
+            />
+            {isDestinationsMenuOpen && (
+              <div className={s.destinations}>
+                <div className={s.destinationsWrapper}>
+                  {destinations &&
+                    Object.entries(destinations).map(
+                      ([continent, countries]) => (
+                        <div className={s.continent} key={continent}>
+                          <div className={s.wrapper}>
+                            <h3>
+                              {continent.charAt(0).toUpperCase() +
+                                continent.slice(1).toLowerCase()}
+                            </h3>
+                            <ul>
+                              {countries.map((country) => (
+                                <li key={country}>
+                                  <Link
+                                    aria-label={`Read more about ${country}`}
+                                    href={`/${country.replace(" ", "-")}`}
+                                  >
+                                    {capitalize(country)}
+                                  </Link>
+                                </li>
+                              ))}
+                            </ul>
+                          </div>
+                        </div>
+                      ),
+                    )}
+                </div>
               </div>
-              {/* <div className={s.popularDestinations}>
-                <Link href="/" className={s.viewAll}>
-                  View All Destinations
-                  <FontAwesomeIcon icon={faArrowRight} className={s.icon} />
-                </Link>
-              </div> */}
-            </div>
+            )}
           </div>
         </li>
         <li>
